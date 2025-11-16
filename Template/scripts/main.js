e2a26917148b74f923e011f1198e8cdffb103bbc
Yu-Gitter <yadomini.7274.strator@gmail.com>
2025-11-16 18:50:21 +0900

    main.js を更新

--- Template/scripts/main.js
+++ Template/scripts/main.js
@@ -37,10 +37,24 @@ function show_form_2(player){
             return;
         }
         player.sendMessage("試合時間は" + String(response.formValues[0]) + "秒です");
-        player.sendMessage("試合の管理者は" + String(response.formValues[1]) + "です");
+        if (response.formValues[1] == 0){
+            player.sendMessage("試合の管理者は" + String(names[0]) + "です");
+        }else if (response.formValues[1] == 1){
+            player.sendMessage("試合の管理者は" + String(names[1]) + "です");
+        }else if (response.formValues[1] == 2){
+            player.sendMessage("試合の管理者は" + String(names[2]) + "です");
+        }else if (response.formValues[1] == 3){
+            player.sendMessage("試合の管理者は" + String(names[3]) + "です");
+        }else if (response.formValues[4] == 1){
+            player.sendMessage("試合の管理者は" + String(names[4]) + "です");
+        }else {
+            player.sendMessage("試合管理者オーバーフローエラー/管理者の取得に失敗しました(内部では記録されています)");
+        }
     }
 }
+//Over
 
+//フォーム起動監視
 server.world.afterEvents.itemUse.subscribe(ev -> {
     if(ev.itemStack.typeId == "minecraft:lapis_lazuli"){
         show_form_2(ev.source);
